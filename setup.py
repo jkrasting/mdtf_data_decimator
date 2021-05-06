0eb49dba84c69b0274ddfa0f48ff04b0c1bf81f2
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2021-05-06 16:31:24 -0400

    updated setup scripts added information about new cli options and usage in mdtf_synthetic.py to REAME.md renamed core.py to mdtf_synthetic.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ is_travis = "TRAVIS" in os.environ
 
 setup(
     name="mdtf_test_data",
-    version="0.0.1",
+    version="1.0.0",
     author="John Krasting",
     author_email="John.Krasting@noaa.gov",
     description=("Tools for working with MDTF Diagnostics test data sets"),
