3cf268531243d84026b2d6a277a39151419d61c8
Jacob Mims <122570226+jtmims@users.noreply.github.com>
2025-01-21 20:14:05 -0500

    make driver script more universal and add freq cli option

--- mdtf_test_data/util/cli.py
+++ mdtf_test_data/util/cli.py
@@ -5,10 +5,11 @@
 class cli_holder(object):
     "Object with command line info from argparse"
 
-    def __init__(self, convention, startyear, nyears, dlat, dlon, unittest):
+    def __init__(self, convention, startyear, nyears, freq, dlat, dlon, unittest):
         self.convention = convention
         self.startyear = startyear
         self.nyears = nyears
+        self.freq = freq
         self.dlat = dlat
         self.dlon = dlon
         self.unittest = unittest
